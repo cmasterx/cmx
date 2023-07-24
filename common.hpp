475573352505bcd7de8f61c33eaa47721cccf9fe
Charlemagne Wong <charle.cwong@gmail.com>
2023-07-24 00:40:48 -0500

    Add common file inherited by all files in the library

---
+++ common.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "flags.hpp"
+#include <cstdint>
+
+namespace cmx
+{
+
+using libversion_t = uint32_t;
+static constexpr libversion_t LIBVERSION = static_cast<libversion_t>(20230723L);
+
+} // namespace cmx
